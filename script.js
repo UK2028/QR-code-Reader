33840c7d8c7e6a8e49a17111d634061a4ed105e6
udit khandelwal <u.khandelwal02@gmail.com>
2023-08-17 19:48:53 +0530

    second commit

--- script.js
+++ script.js
@@ -8,44 +8,42 @@ let closeButton = document.querySelector("#close-button");
 let output;
 let html;
 
-input.addEventListener("change",fetchInput);
+input.addEventListener("change", fetchInput);
 
-function fetchInput() 
-{
-    html = inputDiv.innerHTML;
-    let formData = new FormData();
-    formData.append("file",input.files[0]);
-    fetchQRCode(formData,input.files[0]);
+function fetchInput() {
+  html = inputDiv.innerHTML;
+  let formData = new FormData();
+  formData.append("file", input.files[0]);
+  fetchQRCode(formData, input.files[0]);
 }
 
-function fetchQRCode(formData,image)
-{
-    uploadDiv.innerText = " Scanning QR Code ";
-    fetch("http://api.qrserver.com/v1/read-qr-code/",{
-        method:"POST",
-        body:formData
-    }).then(res=>res.json())
-    .then(data=>{
-        output = data[0]["symbol"][0]["data"];
-        inputDiv.innerHTML = `<img src="${URL.createObjectURL(image)}">`;
-        outputDiv.style.display = "block";
-        outputText.innerText = output;
+function fetchQRCode(formData, image) {
+  uploadDiv.innerText = " Scanning QR Code ";
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      output = data[0]["symbol"][0]["data"];
+      inputDiv.innerHTML = `<img src="${URL.createObjectURL(image)}">`;
+      outputDiv.style.display = "block";
+      outputText.innerText = output;
     });
-};
+}
 
 copyButton.addEventListener("click", async () => {
-    let p = await navigator.clipboard.writeText(output).then(()=> "Copied");
-    let t = copyButton.innerHTML;
-    copyButton.innerHTML = p;
-    setTimeout(() => {
-        copyButton.innerHTML = t;
-    },1000);
+  let p = await navigator.clipboard.writeText(output).then(() => "Copied");
+  let t = copyButton.innerHTML;
+  copyButton.innerHTML = p;
+  setTimeout(() => {
+    copyButton.innerHTML = t;
+  }, 1000);
 });
 
-closeButton.addEventListener("click",() => {
-    outputDiv.style.display = "none";
-    inputDiv.innerHTML = html;
-    input = document.querySelector("#qr-upload");
-    input.addEventListener("change",fetchInput);
+closeButton.addEventListener("click", () => {
+  outputDiv.style.display = "none";
+  inputDiv.innerHTML = html;
+  input = document.querySelector("#qr-upload");
+  input.addEventListener("change", fetchInput);
 });
-
